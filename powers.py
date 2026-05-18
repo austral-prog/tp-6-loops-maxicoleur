84e2315c4453fb29a06f926e4c6b25b73f101cb5
maxicoleur <maximocoleur@gmail.com>
2026-05-18 13:04:31 -0300

    Update powers.py

--- powers.py
+++ powers.py
@@ -7,8 +7,13 @@ def power(base, exp):
 
     Ejemplo: power(2, 3) -> 8  (2*2*2)
     """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    resultado=1
+    if exp<=0:
+        return 1
+    else:
+        for i in range(exp):
+            resultado=resultado*base
+        return resultado
 
 def sum_of_powers(base, max_exp):
     """
@@ -17,4 +22,7 @@ def sum_of_powers(base, max_exp):
 
     Ejemplo: sum_of_powers(2, 3) -> 15  (1+2+4+8)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    resultado=0
+    for i in range(0,max_exp+1):
+        resultado=resultado+base**i
+    return resultado  # Remove this line and implement
